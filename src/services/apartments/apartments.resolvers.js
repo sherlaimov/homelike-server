990553e90b59efcb4595d31ada272a0625ee3cac
Eugene <sherlaimov@gmail.com>
2019-11-06 15:38:35 +0200

    original commit with users.resolvers.js fixed

---
+++ src/services/apartments/apartments.resolvers.js
@@ -0,0 +1,17 @@
+export default function(Users, Locations) {
+  const apartmentsResolvers = {
+    Apartments: {
+      owner: apartment => {
+        return Users.find({ query: { _id: apartment.owner } }).then(result => result[0]);
+      },
+      location: apartment => {
+        return Locations.find({ query: { _id: apartment.location } }).then(result => result);
+      },
+      details: apartment => {
+        return apartment.detail;
+      },
+    },
+  };
+
+  return apartmentsResolvers;
+}
---
+++ src/services/users/users.resolvers.js
@@ -0,0 +1,11 @@
+export default function(Profiles) {
+  const usersResolvers = {
+    Users: {
+      profile: user => {
+        return Profiles.find({ query: { _id: user.owner } }).then(result => result[0]);
+      },
+    },
+  };
+
+  return usersResolvers;
+}
